55d3b27dcce75a5d170de84d6f0a516c927ac25f
Yoganshbhatt <yoganshbhatt000@gmail.com>
2026-06-14 15:36:14 +0530

    fix: update release hashes for fixed zips

--- mcraw-tui.rb
+++ mcraw-tui.rb
@@ -6,13 +6,13 @@ class McrawTui < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Yoganshbhatt/mcraw-tui/releases/download/v0.1.0/mcraw-tui-aarch64-apple-darwin.zip"
-    sha256 "8fdeaea6b8f72758138987acc0e6cb098531fe9412703f223e27ce4998e137cd"
+    sha256 "BB91AE1BCC06EE3FC5030E2E2FEDAA5A77E059B598EB48D275C7C83134B6D87C"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/Yoganshbhatt/mcraw-tui/releases/download/v0.1.0/mcraw-tui-x86_64-apple-darwin.zip"
-    sha256 "15fcadd5bcaf5d7441ba93a4f165f8e496da0382f2b3bd343d74ecda4e840bbd"
+    sha256 "84E8D5263F6A9F6EBC1C5FC73B2E0B30C1D01E8A3CBDC49003325A68DB2024BD"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/Yoganshbhatt/mcraw-tui/releases/download/v0.1.0/mcraw-tui-x86_64-unknown-linux-gnu.zip"
-    sha256 "adad299bc4fa1f89750e02dc0e5ebe1c77f9160fdc3b610a48a223a39707c1be"
+    sha256 "2FB96B72B72E87F2B864607B36F0E3421F92E4FB824F3F8CAD54CF4D17DDFDD4"
   end
 
   depends_on "ffmpeg"
